fe1d8a883e21991a97a1ea24afd4fab4c2924a3e
Atoche Pascual <jmatochepascual@gmail.com>
2023-06-20 17:57:47 -1000

    [ADD] controlador para viajes

--- controllers/paginasController.js
+++ controllers/paginasController.js
@@ -1,9 +1,28 @@
+import { Viaje } from '../models/Viaje.js';
+
 const paginaNosotros = (req, res) => { // req lo que enviamos : res lo que express nos responde
   res.render('../views/nosotros.pug', {
     pagina: 'Nosotros'
   })
 }
 
+const paginaViajes = async (req, res) => { // req lo que enviamos : res lo que express nos responde
+
+  // Consultamos los viajes en la BD
+  try {
+    const viajes = await Viaje.findAll()
+
+    res.render('../views/viajes.pug', {
+      pagina: 'Próximos Viajes',
+      viajes
+    })
+  } catch (error) {
+    console.log(error);
+  }
+
+}
+
 export {
   paginaNosotros,
+  paginaViajes,
 }
