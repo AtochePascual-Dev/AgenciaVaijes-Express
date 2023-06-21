d0ac5ef3247683646bdb657ddeccf7510747d15d
Atoche Pascual <jmatochepascual@gmail.com>
2023-06-20 19:26:00 -1000

    [ADD] ruta a viaje detalle

--- routers/index.js
+++ routers/index.js
@@ -1,11 +1,13 @@
 import express from 'express';
-import { paginaNosotros, paginaViajes } from '../controllers/paginasController.js'
+import { paginaNosotros, paginaViajes, paginaDetalleViaje } from '../controllers/paginasController.js'
 
 const router = express.Router();
 
 router.get('/nosotros', paginaNosotros);
 
 router.get('/viajes', paginaViajes);
+
+router.get('/viajes/:slug', paginaDetalleViaje);
 
 
 export default router;
